3262a5bf6b8e7fca7e7d80cc49916b87a7316009
alstjd0319 <art010319@naver.com>
2026-05-06 00:17:10 +0900

    login api

--- lib/app/extensions/snackbar_extension.dart
+++ lib/app/extensions/snackbar_extension.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 extension SnackbarExtension on BuildContext {
-  void showSnackbar(String text) {
+  void showSnackbar(String text, {bool isError = false}) {
     ScaffoldMessenger.of(
       this,
     ).showSnackBar(const SnackBar(content: Text('곧 기능을 출시합니다!')));
--- lib/ui/pages/signup/signup_page.dart
+++ lib/ui/pages/signup/signup_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:lucide_icons_flutter/lucide_icons.dart';
+import 'package:unitask/app/extensions/snackbar_extension.dart';
+import 'package:unitask/services/api_service.dart';
 import 'package:unitask/ui/pages/common/label_text_field.dart';
 
 class SignupPage extends StatefulWidget {
@@ -15,6 +18,11 @@ class _MyWidgetState extends State<SignupPage> {
   final TextEditingController _passwordController = .new();
   final TextEditingController _passwordConfirmController = .new();
 
+  bool _loading = false;
+
+  void _startLoading() => setState(() => _loading = true);
+  void _stopLoading() => setState(() => _loading = false);
+
   @override
   void dispose() {
     _nameController.dispose();
@@ -25,18 +33,45 @@ class _MyWidgetState extends State<SignupPage> {
     super.dispose();
   }
 
-  void _onSignup() {
+  Future<void> _onSignup() async {
     debugPrint('계정 만들기');
 
-    final name = _nameController.text;
-    final email = _emailController.text;
-    final password = _passwordController.text;
-    final passwordConfirm = _passwordConfirmController.text;
+    final name = _nameController.text.trim();
+    final email = _emailController.text.trim();
+    final password = _passwordController.text.trim();
+    final passwordConfirm = _passwordConfirmController.text.trim();
 
-    debugPrint('이름: $name');
-    debugPrint('이메일: $email');
-    debugPrint('비밀번호: $password');
-    debugPrint('비밀번호 확인: $passwordConfirm');
+    if (name.isEmpty || email.isEmpty || password.isEmpty) {
+      context.showSnackbar('정보가 올바르지 않습니다.', isError: true);
+      return;
+    }
+
+    if (password != passwordConfirm) {
+      context.showSnackbar('비밀전호가 일치하지 않습니다.', isError: true);
+      return;
+    }
+
+    _startLoading();
+
+    final signupResult = await ApiService.signup(
+      email: email,
+      password: password,
+      name: name,
+    );
+
+    _stopLoading();
+
+    if (signupResult == null) return;
+
+    if (!signupResult) {
+      if (mounted) {
+        context.showSnackbar('계정 생성에 실패했습니다.', isError: true);
+      }
+      return;
+    }
+    if (mounted) {
+      context.pop();
+    }
   }
 
   @override
@@ -48,43 +83,50 @@ class _MyWidgetState extends State<SignupPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(20),
-        child: Column(
-          spacing: 25,
-          children: [
-            LabelTextField(
-              controller: _nameController,
-              label: '이름',
-              icon: LucideIcons.userRoundPen,
-              hintText: '',
-            ),
-            LabelTextField(
-              controller: _emailController,
-              label: '이메일',
-              icon: LucideIcons.mail,
-              hintText: '',
-            ),
-            LabelTextField(
-              controller: _passwordController,
-              label: '비밀번호',
-              enableObscure: true,
-              icon: LucideIcons.lockKeyhole,
-              hintText: '',
-            ),
-            LabelTextField(
-              controller: _passwordConfirmController,
-              label: '비밀번호 확인',
-              enableObscure: true,
-              icon: LucideIcons.lockKeyholeOpen,
-              hintText: '',
-            ),
-            SizedBox(
-              width: .infinity,
-              child: ElevatedButton(
-                onPressed: _onSignup,
-                child: Text('계정 만들기'),
+        child: SingleChildScrollView(
+          child: Column(
+            spacing: 25,
+            children: [
+              LabelTextField(
+                controller: _nameController,
+                label: '이름',
+                icon: LucideIcons.userRoundPen,
+                hintText: '',
               ),
-            ),
-          ],
+              LabelTextField(
+                controller: _emailController,
+                label: '이메일',
+                icon: LucideIcons.mail,
+                hintText: '',
+              ),
+              LabelTextField(
+                controller: _passwordController,
+                label: '비밀번호',
+                enableObscure: true,
+                icon: LucideIcons.lockKeyhole,
+                hintText: '',
+              ),
+              LabelTextField(
+                controller: _passwordConfirmController,
+                label: '비밀번호 확인',
+                enableObscure: true,
+                icon: LucideIcons.lockKeyholeOpen,
+                hintText: '',
+              ),
+              SizedBox(
+                width: .infinity,
+                child: ElevatedButton(
+                  onPressed: _onSignup,
+                  child: _loading
+                      ? const SizedBox.square(
+                          dimension: 30,
+                          child: CircularProgressIndicator(color: Colors.white),
+                        )
+                      : const Text('계정 만들기'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
